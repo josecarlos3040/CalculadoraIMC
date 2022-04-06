0e74b5a7e35f5597e389c2b76a8741241dda2e27
José Carlos <jose.viveiros@aluno.ifsp.edu.br>
2022-04-06 08:41:41 -0300

    Atualização do csproj

--- Program.cs
+++ Program.cs
@@ -19,7 +19,33 @@ namespace CalculadoraIMC
 
             Console.WriteLine("Nome: {0}\nMassa: {1}\nAltura: {2}\nIMC: {3}", nome, massa, altura, imc);
 
-
+             if(imc <= 18.5)
+            {
+                Console.WriteLine("Peso abaixo do normal");
+            }
+            else if ((imc > 18.5) && (imc < 25))
+            {
+                Console.WriteLine("Peso normal ");
+            }
+            else if ((imc >= 25) && (imc < 30))
+            {
+                Console.WriteLine("Sobre o Peso");
+            }
+            else if ((imc >= 30) && (imc < 35))
+            {
+                Console.WriteLine("1° de Obesidade");
+            }
+            else if ((imc >= 35) && (imc < 40))
+            {
+                Console.WriteLine("2° de Obesidade");
+            }
+            else if (imc >= 40)
+            {
+                Console.WriteLine("3° de Obesidade");
+            }
         }
     }
 }
+        
+    
+
